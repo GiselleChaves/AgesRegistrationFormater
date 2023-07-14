a50497306a0daed2acde2303a54537d003bd3aaf
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-07-14 09:41:13 -0300

    Atualização da divisão de alunos por nível de Ages e escrita no doc Csv.

--- src/Project.java
+++ src/Project.java
@@ -27,6 +27,38 @@ public class Project {
   }  
 
   public Student[] getStudentsArray(){
-    return (Student[]) students.toArray();
+    Student[] studentsArray = new Student[size()];
+    for(int i=0; i<students.size();i++){
+      studentsArray[i] = students.get(i);
+    }
+    return studentsArray;
+  }
+
+  public static int getBiggestLevelSize(LinkedList<Project> projects, String level){
+    int quantity = 0, maior = 0;
+    
+    for(Project projectsInterate:projects){
+      quantity = 0;
+      for(Student student:projectsInterate.getStudentsArray()){
+        if(student.getLevel().equals(level)){
+          quantity++;  
+        }
+        if(maior < quantity){
+          maior = quantity;
+        }
+      }
+    }
+    return maior;
+  }
+
+  public static int levelSize(LinkedList<Project> projects, String level){
+    if(level.equals("Ages I")){
+      return 1;
+    }else if(level.equals("Ages II")){
+      return getBiggestLevelSize(projects, "Ages I") + 1;
+    }else if(level.equals("Ages III")){
+      return (getBiggestLevelSize(projects, "Ages I") + getBiggestLevelSize(projects, "Ages II") + 1);
+    }else
+      return getBiggestLevelSize(projects, "Ages I") + getBiggestLevelSize(projects, "Ages II") + getBiggestLevelSize(projects, "Ages III");
   }
 }
